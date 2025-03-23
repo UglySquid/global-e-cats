2d40d0fadacd31cf62d0d4f75431c5ab7e93091d
UglySquid <christine.wei.63@gmail.com>
2025-03-23 00:21:58 -0400

    vercel time

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const outDir = resolve(__dirname, 'dist')
 export default defineConfig({
   root,
   plugins: [reactRefresh()],
-  base: "/global-economics-DA/",
+  base: "./",
   build: {
     outDir,
     emptyOutDir: true,
